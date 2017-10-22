13f27aa7e707014009770eeab5595ec21c3e845e
S.M.Imtiaz <smimtiaz.cse@gmail.com>
2017-10-23 05:38:23 +0600

    Upload General Image

--- index.php
+++ index.php
@@ -5,19 +5,45 @@ include 'lib/Database.php';
 
 $db = new Database();
 ?>
-			<div class="myform">
-				<form action="" method="POST" enctype="multipart/form-data">
-					<table>
-						<tr>
-							<td>Select Image</td>
-							<td> <input type="file" name="image"> </td>
-						</tr>
-						<tr>
-							<td></td>
-							<td> <input type="submit" name="submit" value="Upload"> </td>
-						</tr>
-					</table>
-				</form>
-			</div>
+
+
+<div class="myform">
+
+	<?php
+		if (isset($_POST['submit'])) {
+			$permitted = array('jpg','jpeg','png','gif');
+			$img_Name = $_FILES['image']['name'];
+			$img_size = $_FILES['image']['size'];
+			$img_temp = $_FILES['image']['tmp_name'];
+			$folder = "uploads/";
+
+			move_uploaded_file($img_temp, $folder.$img_Name);
+			$query = "INSERT INTO tbl_image(image) VALUES ('$img_Name')";
+			$insert = $db->insert($query);
+			if ($insert) {
+				echo "<span class='success'>Image Uploaded Successfully!!</span>";
+			}else{
+				echo "<span class='error'>Image Not Uploaded Successfully!!</span>";
+			}
+
+		}
+	?>
+
+
+<form action="" method="POST" enctype="multipart/form-data">
+	<table>
+		<tr>
+			<td>Select Image</td>
+			<td> <input type="file" name="image"> </td>
+		</tr>
+		<tr>
+			<td></td>
+			<td> <input type="submit" name="submit" value="Upload"> </td>
+		</tr>
+	</table>
+</form>
+</div>
+
+
 
 <?php include 'inc/footer.php'; ?>
